cc1b276aae3bee4d84343a9c7d653b52c8863a8e
Strauss (Air) <dstrauss85@gmail.com>
2012-06-07 14:40:21 -0700

    plus mpi

--- src/mpiUmfTest.py
+++ src/mpiUmfTest.py
@@ -8,9 +8,14 @@ import numpy
 import scipy.sparse
 import scipy.sparse.linalg as lin
 import time
+from mpi4py import MPI
 
 
 def main():
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    nProc = comm.Get_size()
+    
     A = numpy.random.randn(5000,5000)
     A[A<0.5] = 0
     
@@ -20,7 +25,7 @@ def main():
         ti = time.time()
         
         Z = lin.factorized(M)
-        print repr(ix) + ' exec time = ' + repr(time.time()-ti)
+        print 'rank ' + repr(rank) + ' iter ' + repr(ix) + ' exec time = ' + repr(time.time()-ti)
         
 if __name__ == "__main__":
     main()
