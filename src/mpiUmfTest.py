96543a00fb4480ab34ec6f3ab10808d1d9a6181a
Strauss (Air) <dstrauss85@gmail.com>
2012-06-07 15:19:28 -0700

    what if it goes to a txt file instead of stdout?

--- src/mpiUmfTest.py
+++ src/mpiUmfTest.py
@@ -12,20 +12,24 @@ from mpi4py import MPI
 
 
 def main():
-    #comm = MPI.COMM_WORLD
-    #rank = comm.Get_rank()
-    #nProc = comm.Get_size()
-    rank = 5
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    # nProc = comm.Get_size()
+    # rank = 5
     A = numpy.random.randn(5000,5000)
     A[A<0.5] = 0
     
     M = scipy.sparse.csc_matrix(A)
     
-    for ix in range(10000):
+    f = open('./notes' + repr(rank) + '.txt', 'w')
+    
+    
+    for ix in range(2):
         ti = time.time()
-        
         Z = lin.factorized(M)
-        print 'rank ' + repr(rank) + ' iter ' + repr(ix) + ' exec time = ' + repr(time.time()-ti)
+        f.write('rank ' + repr(rank) + ' iter ' + repr(ix) + ' exec time = ' + repr(time.time()-ti))
+    
+    f.close()
         
 if __name__ == "__main__":
     main()
