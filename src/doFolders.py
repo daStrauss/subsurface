e4f72f2d46b09ea80d19fea98902e53b0075f1a6
Strauss (Air) <dstrauss85@gmail.com>
2012-06-11 18:07:18 -0700

    ok - made a do folders routine so that I don't have to worry about interal methods failing.

---
+++ src/doFolders.py
@@ -0,0 +1,46 @@
+'''
+Created on Jun 11, 2012
+
+@author: dstrauss
+'''
+
+import sys
+import os
+
+def traceBuild(baseTag):
+    if not os.path.exists(baseTag):
+            os.mkdir(baseTag)
+
+    for ix in range(100):
+        outDir = baseTag + '/trial' + repr(ix) + '/'
+        if not os.path.exists(outDir):
+            os.mkdir(outDir)
+            
+        figDir = outDir + 'Figs/'
+        datDir = outDir + 'Data/'
+        if not os.path.exists(figDir):
+            os.mkdir(figDir)
+        if not os.path.exists(datDir):
+            os.mkdir(datDir)
+
+def main():
+    ''' simple main routine '''
+    if len(sys.argv) == 1:
+        print 'I think you meant to specify one of the following:'
+        print 'splitField'
+        print 'contrastX'
+        print 'sba'
+        
+    elif sys.argv[1] == 'splitField':
+        traceBuild('splitField')
+
+    elif sys.argv[1] == 'contrastX':
+        traceBuild('contrastX')
+        
+    elif sys.argv[1] == 'sba':
+        traceBuild('sba')
+    else: 
+        print 'I think you asked for the wrong thing:'
+ 
+if __name__ == "__main__":
+    main()
