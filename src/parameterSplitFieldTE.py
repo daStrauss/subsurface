e12c114f168b817f4e183c94137ccd37a2339036
Strauss (Air) <dstrauss85@gmail.com>
2012-07-16 16:46:11 -0700

    have routine now to search over all the internal parameters in the splitFieldForm. 5x

---
+++ src/parameterSplitFieldTE.py
@@ -0,0 +1,25 @@
+'''
+Created on Jul 16, 2012
+
+@author: dstrauss
+'''
+
+import numpy as np
+
+
+D = {'solverType':'splitField', 'flavor':'TE', 'numRuns':500, 'expt':'intParameters'}
+
+
+def getMyVars(parseNumber, D):
+    '''routine to return the parameters to test at the current iteration.'''
+    rhos, xis = np.meshgrid(np.logspace(2,4,10), np.logspace(-4,-2,10))
+    rhos = rhos.flatten()
+    xis = xis.flatten()
+    
+    
+    
+    D['rho'] = rhos[parseNumber%100] 
+    D['xi'] = xis[parseNumber%100]
+    D['bkgNo'] = int(parseNumber/100)
+    
+    return D
