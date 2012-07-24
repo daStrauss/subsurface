dffb6a53d2497ffe2bc0c052de305d548ca68186
Strauss (Air) <dstrauss85@gmail.com>
2012-07-24 08:32:23 -0700

    changed to procs from nodes. lets hope that changes things.

--- src/manageTasks.py
+++ src/manageTasks.py
@@ -96,7 +96,7 @@ def main():
             fid = open(fileName, 'w')
             fid.write('mpiexec -wdir /shared/users/dstrauss/subsurface/src python coordinate.py ' + sys.argv[1] + ' ' + repr(ix))
             fid.close()
-            cmd = ['qsub', '-N', jobTitle, '-l' , 'walltime=10:00:00', '-l','nodes=' + repr(lclD['numProcs']), '-l', 'nice=0', fileName]        
+            cmd = ['qsub', '-N', jobTitle, '-l' , 'walltime=10:00:00', '-l','procs=' + repr(lclD['numProcs']), '-l', 'nice=0', fileName]        
             print cmd
             
             jobList.append(submitJob(cmd))
