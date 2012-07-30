aff31ec0fe6dd9b20066964347fab520b098aff2
Strauss (Air) <dstrauss85@gmail.com>
2012-07-30 14:09:56 -0700

    going to do a search through the parameter space of rho for the contrastX form.

---
+++ src/parameterContrastX.py
@@ -0,0 +1,24 @@
+'''
+Created on Jul 30, 2012
+
+@author: dstrauss
+'''
+
+import numpy as np
+
+
+D = {'solverType':'contrastX', 'flavor':'TE', 'numRuns':100, 'expt':'intParameters'}
+
+
+def getMyVars(parseNumber, D):
+    '''routine to return the parameters to test at the current iteration.'''
+    rhos, bkgLocal = np.meshgrid(np.logspace(-4,0,20), range(5))
+    rhos = rhos.flatten()
+    bkgLocal = bkgLocal.flatten()
+    
+    
+    
+    D['rho'] = rhos[parseNumber] 
+    D['bkgNo'] =  bkgLocal[parseNumber] + 100
+    
+    return D
