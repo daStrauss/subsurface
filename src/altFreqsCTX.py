196e3da0b82b4b8231294e0f3daa856b85acd5cf
Strauss (Air) <dstrauss85@gmail.com>
2012-09-14 11:32:34 -0700

    added num frequencies arg? I don't remember that one.

--- src/altFreqsCTX.py
+++ src/altFreqsCTX.py
@@ -13,4 +13,5 @@ def getMyVars(parseNumber, D):
     '''routine to return the parameters to test at the current iteration.'''
     D['bkgNo'] = parseNumber + 100   
     D['freqs'] = np.array([0.2e3, 6e3, 12e3, 24e3])
+    D['numProcs'] = len(D['freqs'])*len(D['inc'])
     return D
