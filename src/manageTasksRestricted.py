7cdb73ceb8888e5bf6f1e0a4b72f089d11452097
Strauss (Air) <dstrauss85@gmail.com>
2012-10-25 13:10:08 -0700

    trying proc bitmap?x

--- src/manageTasksRestricted.py
+++ src/manageTasksRestricted.py
@@ -113,9 +113,9 @@ def main():
                 fileName = 'sub' + sys.argv[1] + repr(ix) + '.pbs'
         
                 fid = open(fileName, 'w')
-                fid.write('mpiexec -npernode 1' + ' -wdir /shared/users/dstrauss/dev/subsurface/src python coordinate.py ' + sys.argv[1] + ' ' + repr(ix))
+                fid.write('mpiexec' + ' -wdir /shared/users/dstrauss/dev/subsurface/src python coordinate.py ' + sys.argv[1] + ' ' + repr(ix))
                 fid.close()
-                cmd = ['qsub', '-N', jobTitle, '-l' , 'walltime=20:00:00', '-l','nodes=' + repr(nProcs) + ':ppn=4', '-l', 'nice=0', fileName]        
+                cmd = ['qsub', '-N', jobTitle, '-l' , 'walltime=20:00:00', '-l','nodes=' + repr(nProcs) + ':ppn=4', '-l', 'nice=0','-l','procs_bitmap=1000', fileName]        
                 print cmd
             
                 jobList.append(submitJob(cmd))
