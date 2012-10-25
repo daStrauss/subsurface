4ec8a07cdea8c6e80bbd000ee9fb91e7b7d7b5ae
Strauss (Air) <dstrauss85@gmail.com>
2012-10-25 10:13:56 -0700

    ready to go with some projection management.

---
+++ src/parameterProjection.py
@@ -0,0 +1,30 @@
+'''
+Created on Oct 25, 2012
+
+@author: dstrauss
+'''
+
+import numpy as np
+
+
+D = {'solverType':'projection', 'flavor':'TE', 'numRuns':1, 'expt':'intParameters'}
+
+
+def getMyVars(parseNumber, D):
+    '''routine to return the parameters to test at the current iteration.'''
+    rhos, bkno = np.meshgrid(np.logspace(-8,0,20), np.arange(5))
+    rhos = rhos.flatten()
+    bkno = bkno.flatten()
+    
+    
+    
+    D['rho'] = rhos[parseNumber%100] 
+    # D['xi'] = xis[parseNumber%100]
+    D['bkgNo'] = bkno[parseNumber] + 100
+    
+    D['freqs'] = np.array([1e3, 3e3, 13e3, 25e3])  
+    D['inc'] = np.array([75])*np.pi/180
+    D['numProcs'] = 4
+    D['maxIter'] = 200
+    
+    return D
