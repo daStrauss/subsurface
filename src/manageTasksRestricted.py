f472012e62cbb16e4d43ff42c9e9c370c3673500
Strauss (Air) <dstrauss85@gmail.com>
2012-10-25 17:37:05 -0700

    be specific with submission?

--- src/manageTasksRestricted.py
+++ src/manageTasksRestricted.py
@@ -113,9 +113,9 @@ def main():
                 fileName = 'sub' + sys.argv[1] + repr(ix) + '.pbs'
         
                 fid = open(fileName, 'w')
-                fid.write('mpiexec' + ' -wdir /shared/users/dstrauss/dev/subsurface/src python coordinate.py ' + sys.argv[1] + ' ' + repr(ix))
+                fid.write('mpiexec -npernode 1 ' + ' -wdir /shared/users/dstrauss/dev/subsurface/src python coordinate.py ' + sys.argv[1] + ' ' + repr(ix))
                 fid.close()
-                cmd = ['qsub', '-N', jobTitle, '-l' , 'walltime=20:00:00', '-l','procs=' + repr(nProcs), '-l', 'nice=0','-l','procs_bitmap=1000', fileName]        
+                cmd = ['qsub', '-N', jobTitle, '-l' , 'walltime=20:00:00', '-l','nodes=' + repr(nProcs) + ':ppn=4', fileName]        
                 print cmd
             
                 jobList.append(submitJob(cmd))
