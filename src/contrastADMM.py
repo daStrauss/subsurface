97503cf99e35a53f2944c16310a0a2cc39f6a67f
Strauss (Air) <dstrauss85@gmail.com>
2012-11-08 09:55:55 -0800

    fwd not fws.

--- src/contrastADMM.py
+++ src/contrastADMM.py
@@ -182,7 +182,7 @@ class problem(optimizer):
         D = {'f':self.fwd.f, 'angle':self.fwd.incAng, 'sigMat':sgmm[0], 'ub':ub[0], \
              'us':us[0], 'uTrue':uTrue[0], \
              'X':self.X, 'obj':self.obj, 'flavor':self.fwd.flavor, 'gap':self.gap, \
-             'obj':self.objInt, 'Ms':self.fws.Ms}
+             'obj':self.objInt, 'Ms':self.fwd.Ms}
         
         spio.savemat(self.outDir + 'Data/contrastX' + repr(rank) + '_' + repr(ix), D)
         
