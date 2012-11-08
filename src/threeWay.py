bb531dffe473bcb39113984ddc62b4e1a987611b
Strauss (Air) <dstrauss85@gmail.com>
2012-11-08 11:45:15 -0800

    uh dictionary key wrong.gi

--- src/threeWay.py
+++ src/threeWay.py
@@ -14,7 +14,7 @@ def getMyVars(parseNumber, D):
     if (parseNumber == 0):
         D['freqs'] = np.array([1e3])
         D['numProcs'] = 1
-        ['numSensors'] = 400
+        D['numSensors'] = 400
     elif (parseNumber == 1):
         D['freqs'] = np.array([1e3, 25e3])
         D['numProcs'] = 2
