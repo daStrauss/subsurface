68b65b287b95e841f1241f4b35eaa736b2c0eac9
Strauss (Air) <dstrauss85@gmail.com>
2012-11-14 10:54:40 -0800

    dimension analysis? is p2x right?

--- src/forward/te3d.py
+++ src/forward/te3d.py
@@ -197,6 +197,7 @@ class solver(fwd):
         self.p2x = sparse.eye(self.nRx*self.nRy*self.nRz,self.nRx*self.nRy*self.nRz)
         self.p2x = sparse.vstack((self.p2x,self.p2x,self.p2x))
         
+        print 'p2x size  ' + repr(self.p2x.shape)
         if not(hasattr(self, 'x2u')):
             print 'Oh youre hosed now!'
         
