b3cb55b8a4fffb1cff15558854c2ea9dbf67c44d
Strauss (Air) <dstrauss85@gmail.com>
2012-11-14 17:59:52 -0800

    ?

--- src/omg3d.py
+++ src/omg3d.py
@@ -9,7 +9,7 @@ Simple routine to do optimization over 3D!
 
 
 import numpy as np
-D = {'solverType':'contrastX', 'flavor':'TE3D', 'numRuns':1, 'expt':'huge'}
+D = {'solverType':'contrastX', 'flavor':'TE3D', 'numRuns':1, 'expt':'huge', 'numProcs':2}
 
 
 def getMyVars(parseNumber, D):
