f557c093a29a0478d629f209a1df84199aacab1f
Strauss (Air) <dstrauss85@gmail.com>
2012-11-16 12:04:07 -0800

    go some more iterations and print.xs

--- src/testOptRoutine.py
+++ src/testOptRoutine.py
@@ -18,6 +18,6 @@ def getMyVars(parseNumber, D):
     D['inc'] = np.array([75*np.pi/180])
     D['bkgNo'] =  100
     D['numProcs'] = 1
-    D['maxIter'] = 2
+    D['maxIter'] = 10
     
     return D
