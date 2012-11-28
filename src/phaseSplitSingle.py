624b6a5735ae450ebc41889f4e97bc5ff37c823e
Strauss (Air) <dstrauss85@gmail.com>
2012-11-28 14:18:18 -0800

    print out the right stuff.

--- src/phaseSplitSingle.py
+++ src/phaseSplitSingle.py
@@ -125,7 +125,8 @@ class problem(optimizer):
             
         D = {'f':self.fwd.f, 'angle':self.fwd.incAng, 'sigMat':sgmm[0], 'ub':ub[0], \
              'us':us[0], 'uTrue':uTrue[0], \
-             'X':self.X, 'obj':self.obj, 'flavor':self.fwd.flavor, 'gap':self.gap, \
+             'rt':self.rt, 'rd':self.rd, 'zd':self.zd, 'zt':self.zt, \             
+             'obj':self.obj, 'flavor':self.fwd.flavor, 'gap':self.gap, \
              'obj':self.objInt, 'Ms':self.fwd.Ms, 'phist':self.pL}
         
         spio.savemat(self.outDir + 'Data/contrastX' + repr(rank) + '_' + repr(ix), D)
