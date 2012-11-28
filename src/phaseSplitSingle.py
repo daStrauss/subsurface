8d02184c957f1b8c82ed88bc88608e30465dbe79
Strauss (Air) <dstrauss85@gmail.com>
2012-11-28 14:13:39 -0800

    likewise with Md.

--- src/phaseSplitSingle.py
+++ src/phaseSplitSingle.py
@@ -136,7 +136,7 @@ class problem(optimizer):
         Revised to be just a simple aggregation/mean step '''
         
         ''' nothing to do here really'''
-        P = self.rt/np.abs(self.s*self.Md.T*(self.us+self.ub))
+        P = self.rt/np.abs(self.s*self.fwd.Md.T*(self.us+self.ub))
         return P
         
         
