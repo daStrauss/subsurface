0e6ef41da0f744c2787e53c40867c77edf21522d
Strauss (Air) <dstrauss85@gmail.com>
2012-11-29 13:03:08 -0800

    oh. inc angle makes a difference. does the # of frequencies make a difference w.r.t optimal parameters?

--- src/parametersCTX6.py
+++ src/parametersCTX6.py
@@ -7,24 +7,29 @@ Created on Nov 8, 2012
 import numpy as np
 
 
-D = {'solverType':'contrastX', 'flavor':'TE', 'numRuns':100, 'expt':'newParams6', 'numProcs':6}
+D = {'solverType':'phaseSplit', 'flavor':'TE', 'numRuns':245, 'expt':'newParams2', 'numProcs':6}
 
 
 def getMyVars(parseNumber, D):
     '''routine to return the parameters to test at the current iteration.'''
-    rhos, bkgLocal = np.meshgrid(np.logspace(-4,0,20), range(5))
+    rhos, xis, bkgLocal = np.meshgrid(np.logspace(-6,-3,7), np.logspace(-14,-11,7), range(5))
     rhos = rhos.flatten()
+    xis = xis.flatten()
     bkgLocal = bkgLocal.flatten()
     
+        
     D['freqs'] = np.linspace(1e3,25e3,6)
     D['numProcs'] = 6
     D['numSensors'] = 400
-        
-    D['lam'] = 1e-8
-    # D['rho'] = 1e-3    
+
+    D['lam'] = 0
+#    D['rho'] = 1e-3    
     D['inc'] = np.array([75*np.pi/180])
+#    D['maxIter'] = 200
+
+    
     
     D['rho'] = rhos[parseNumber] 
+    D['xi'] = xis[parseNumber]
     D['bkgNo'] =  bkgLocal[parseNumber] + 100
-    D['numProcs'] = 6
     return D
