174fa2426bae3b574c95c46c72fee1d4a28c650d
Strauss (Air) <dstrauss85@gmail.com>
2012-12-06 18:51:48 -0800

    or maybe it goes there

--- src/manageTasksNew.py
+++ src/manageTasksNew.py
@@ -116,7 +116,7 @@ def main():
                 fid = open(fileName, 'w')
                 fid.write('mpiexec -npernode ' + repr(1) + ' --mca btl_tcp_if_include "p3p2" -wdir /shared/users/dstrauss/dev/subsurface/src python coordinate.py ' + sys.argv[1] + ' ' + repr(ix))
                 fid.close()
-                cmd = ['qsub', '-N', jobTitle, '-l' , 'walltime=20:00:00', '-l','nodes=' + repr(nProcs), '-l', 'nice=0', '-q','batchnew', fileName]        
+                cmd = ['qsub', '-N', jobTitle, '-l' , 'walltime=20:00:00', '-l','nodes=1:ppn=' + repr(nProcs), '-l', 'nice=0', '-q','batchnew', fileName]        
                 print cmd
             
                 jobList.append(submitJob(cmd))
