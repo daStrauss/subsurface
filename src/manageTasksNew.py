0cb8a2b36d55d30f7c0bd95a105b422e52efd7c4
Strauss (Air) <dstrauss85@gmail.com>
2012-12-10 18:16:43 -0800

    all back to non-dev.

--- src/manageTasksNew.py
+++ src/manageTasksNew.py
@@ -115,7 +115,7 @@ def main():
                 fileName = 'sub' + sys.argv[1] + repr(ix) + '.pbs'
         
                 fid = open(fileName, 'w')
-                fid.write('mpiexec -npernode ' + repr(1) + ' --mca btl_tcp_if_include "p3p2" -wdir /shared/users/dstrauss/dev/subsurface/src python coordinate.py ' + sys.argv[1] + ' ' + repr(ix))
+                fid.write('mpiexec -npernode ' + repr(1) + ' --mca btl_tcp_if_include "p3p2" -wdir /shared/users/dstrauss/subsurface/src python coordinate.py ' + sys.argv[1] + ' ' + repr(ix))
                 fid.close()
                 cmd = ['qsub', '-N', jobTitle, '-l' , 'walltime=20:00:00', '-l','nodes=1:ppn=' + repr(nProcs), '-l', 'nice=0', '-q','batchnew', fileName]        
                 print cmd
