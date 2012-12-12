c2e5f4a4ddad8a7c74ab183e013dc3f141d2d893
Strauss (Air) <dstrauss85@gmail.com>
2012-12-12 12:33:21 -0800

    ready to go with the SPF again. accidently made changes in CTX.

--- src/expts/param3dSPF.py
+++ src/expts/param3dSPF.py
@@ -10,19 +10,19 @@ D = {'solverType':'splitField', 'flavor':'TE3D', 'numRuns':125, 'expt':'hugeParm
 
 def getMyVars(parseNumber, D):
     
-    rhos,reg,frq = np.meshgrid(np.logspace(-4,4,5), np.logspace(-10,-4,5), np.logspace(3,5,5))
+    rhos,xis,frq = np.meshgrid(np.logspace(-4,4,5), np.logspace(-10,-4,5), np.logspace(3,5,5))
     rhos = rhos.flatten()
     frq = frq.flatten()
-    reg = reg.flatten()
+    xis = xis.flatten()
     
     
     D['inc'] = np.array([75*np.pi/180])
     D['bkgNo'] =  0
     D['numProcs'] = 1
-    D['reg'] = reg[parseNumber]
+    D['xi'] = xis[parseNumber]
     D['rho'] = rhos[parseNumber]
     D['freqs'] = np.array(frq[parseNumber])
-    D['maxIters'] = 200
+#    D['maxIter'] = 1000
     
     return D 
 
