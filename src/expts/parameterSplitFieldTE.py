1ead32c4fa9b8b05763bae277d1e1f90b28e25c5
Strauss (Air) <dstrauss85@gmail.com>
2013-03-19 15:53:17 -0700

    and splitField Tm too.

--- src/expts/parameterSplitFieldTE.py
+++ src/expts/parameterSplitFieldTE.py
@@ -7,12 +7,12 @@ Created on Jul 16, 2012
 import numpy as np
 
 
-D = {'solverType':'splitField', 'flavor':'TE', 'numRuns':500, 'expt':'intParameters'}
+D = {'solverType':'splitField', 'flavor':'Tm', 'numRuns':800, 'expt':'intParameters'}
 
 
 def getMyVars(parseNumber, D):
     '''routine to return the parameters to test at the current iteration.'''
-    rhos, xis = np.meshgrid(np.logspace(2,4,10), np.logspace(-4,-2,10))
+    rhos, xis = np.meshgrid(np.logspace(1,5,10), np.logspace(-5,-1,10))
     rhos = rhos.flatten()
     xis = xis.flatten()
     
